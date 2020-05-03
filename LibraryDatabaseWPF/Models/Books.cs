2001b29dc248a158c8e9db0d347cc3400dfc7060
Samuel Brunner <sbrunner5124@ksu.edu>
2020-05-03 14:56:27 -0500

    Added data structures / constructors for tables

---
+++ LibraryDatabaseWPF/Models/Books.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDatabaseWPF.Models
+{
+    public class Books
+    {
+        public int BookId { get; }
+        public string ISBN { get; }
+        public int AuthorId { get; }
+        public string Title { get; }
+        public string GenreName { get; }
+        public string ConditionType { get; }
+
+
+        public Books(int bookId, string isbn, int authorId, string title, string genreName, string conditionType)
+        {
+            BookId = bookId;
+            ISBN = isbn;
+            AuthorId = authorId;
+            Title = title;
+            GenreName = genreName;
+            ConditionType = conditionType;
+        }
+
+    }
+}
---
+++ LibraryDatabaseWPF/Models/ConditionType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDatabaseWPF.Models
+{
+    public enum ConditionType
+    {
+        New,
+        Worn,
+        Replace
+    }
+}
