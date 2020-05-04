b5334a37f3c9028df1d617264312e5e4c4075356
Samuel Brunner <sbrunner5124@ksu.edu>
2020-05-03 20:43:38 -0500

    Added GetBookById method

--- LibraryDatabaseWPF/Procedures/Library.GetGenreNameFromId.sql
+++ LibraryDatabaseWPF/Procedures/Library.GetGenreNameFromId.sql
@@ -1,9 +1,9 @@
 /*** Returns the GenreId associated with the provied Genre name. Throws an error if the Genre can't be found
 ****/
-DROP PROCEDURE IF EXISTS [Library].GetAuthorNameFromId
+DROP PROCEDURE IF EXISTS [Library].GetGenreNameFromId
 GO
 
-CREATE PROCEDURE [Library].GetAuthorNameFromId
+CREATE PROCEDURE [Library].GetGenreNameFromId
 @GenreId INT
 AS
 
