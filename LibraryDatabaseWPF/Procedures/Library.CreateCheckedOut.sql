4804191c1643a0da9fcf7431fee0b367b89e1b1c
Samuel Brunner <sbrunner5124@ksu.edu>
2020-05-03 23:49:36 -0500

    Added FetchTransactionById procedure and removed references to TranasctionId in CreateCheckedOut procedure

--- LibraryDatabaseWPF/Procedures/Library.CreateCheckedOut.sql
+++ LibraryDatabaseWPF/Procedures/Library.CreateCheckedOut.sql
@@ -1,11 +1,9 @@
 CREATE OR ALTER PROCEDURE Library.CreateCheckedOut
    @BookId INT,
-   @UserId INT,
-   @TransactionId INT OUTPUT
+   @UserId INT
 AS
 
 INSERT Library.CheckedOut(BookId, UserId)
 VALUES(@BookId, @UserId);
 
-SET @TransactionIdId = SCOPE_IDENTITY();
 GO
